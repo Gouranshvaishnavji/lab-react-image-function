9e1996a572945098d61d8e1a92882484aac7c305
Gouranshvaishnavji <gouransh.v@kalvium.community>
2024-03-08 10:41:47 +0530

    error update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,38 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-// import React, {useState} from "react";
 
-//<!---------------uncommented code-------------->
 const Carousel = () => {
   const [count, setCount] = useState(0);
 
   const decrement = () => {
-      if (count < 2) {
-          setCount(count + 1);
-      }
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(images.length - 1); // Loop back to the last image when reaching the beginning
+    }
   };
 
   const increment = () => {
-      if (count > 0) {
-          setCount(count - 1);
-      }
+    if (count < images.length - 1) {
+      setCount(count + 1);
+    } else {
+      setCount(0); // Loop back to the first image when reaching the end
+    }
   };
 
   return (
-      <div className="container">
-          <div className="sidebtn" onClick={increment}><ArrowBackIosIcon /></div>
-          <div className="imgimg">
-              <img src={images[count].img} alt="img" />
-              <h1 className="texter">{images[count].title}</h1>
-              <h3 className="texter">{images[count].subtitle}</h3>
-          </div>
-          <div className="sidebtn" onClick={decrement}><ArrowForwardIosIcon /></div>
+    <div className="container">
+      <div className="sidebtn" onClick={decrement}><ArrowBackIosIcon /></div>
+      <div className="imgimg">
+        <img src={images[count].img} alt="img" />
+        <h1 className="texter">{images[count].title}</h1>
+        <h3 className="texter">{images[count].subtitle}</h3>
       </div>
+      <div className="sidebtn" onClick={increment}><ArrowForwardIosIcon /></div>
+    </div>
   );
 };
+
+export default Carousel;
+
 // <------------------------commented code------------------->
 // class Carousel extends Component {
 //     constructor() {
@@ -71,4 +76,4 @@ const Carousel = () => {
 //       }
 // }
 
-export default Carousel;
+// export default Carousel;
